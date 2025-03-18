82c75183e25227c1d69c30bf628c2eb4d8f45117
Dav <127726361+DavM02@users.noreply.github.com>
2025-03-18 16:27:37 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+function generateRandomNumber(min = 1, max = 50) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateRandomName() {
+    return faker.name.findName();
+}
+
+function Person() {
+
+    this.name = generateRandomName()
+    this.age = generateRandomNumber()
+
+
+    this.interval = setInterval(() => {
+        this.age++;
+    }, 1000);
+
+}
+
+let personsArray = [...Array(4)].map(() => new Person());
+
+function cleanUpArray() {
+    personsArray = personsArray.filter(person => {
+        if (person.age >= 40) {
+            clearInterval(person.interval); 
+            return false;
+        }
+        return true;
+    });
+    console.log(personsArray)
+}
+
+function addNewPersons() {
+    personsArray.push(new Person())
+}
+
+
+setInterval(cleanUpArray, 1000);
+
+setInterval(addNewPersons, 2000);
